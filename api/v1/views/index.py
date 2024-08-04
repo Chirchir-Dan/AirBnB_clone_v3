9eb6c21faeff932ec530a9e62ff16944e06cc14f
Chirchir-Dan <1danchirchir@gmail.com>
2024-08-04 12:32:58 +0300

    added test cases

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,23 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns a JSON status."""
-    return jsonify(
-            {
-                "status": "OK"
-            })
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """retrieves the number of each objects  by type"""
+    stats = {
+        'Amenity': storage.count('Amenity'),
+        'City': storage.count('City'),
+        'Place': storage.count('Place'),
+        'Review': storage.count('Review'),
+        'State': storage.count('State'),
+        'User': storage.count('User')
+    }
+    return jsonify(stats)
