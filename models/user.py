670afd1959864e6a849cca5345617babc4828b6a
Dev-Kings <kingasiadavid41@gmail.com>
2024-08-05 18:06:50 +0300

    Added md5 hashing for User password

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,7 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if 'password' in kwargs:
+            password = kwargs['password']
+            kwargs['password'] = md5(password.encode()).hexdigest()
         super().__init__(*args, **kwargs)
